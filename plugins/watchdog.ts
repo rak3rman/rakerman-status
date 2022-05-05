4fc8cb9fa361d5827baeeb3e41ea19c6f354fd28
Radison Akerman <rak3rman@gmail.com>
2022-05-04 22:24:42 -0500

    Testing watchdog.js 5

--- plugins/watchdog.ts
+++ plugins/watchdog.ts
@@ -1,8 +1,22 @@
 export default defineNuxtPlugin(() => {
     console.log("hiyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    function pingServers(event) {
-        console.log(event)
-        return "successful pinging"
+    async function pingServers(event) {
+        // @ts-ignore
+        let services = await SERVICES.list()
+        // Loop through keys and generate payload
+        for (const key of services.keys) {
+            // @ts-ignore
+            let value = await SERVICES.get(key.name, {type: "json"}) // Get service
+            // @ts-ignore
+            await SERVICES.put(value.name, JSON.stringify({
+                is_up: true,
+                is_maintain: value.is_maintain,
+                last_up: Date.now(),
+                last_down: Date.now(),
+                location: value.location,
+                subscribers: value.subscribers,
+            }))
+        }
     }
     addEventListener('scheduled', event => {
         // @ts-ignore
