5051cb36b49395844b3613e28cfeeee31d1e91b3
Radison Akerman <rak3rman@gmail.com>
2022-05-04 23:17:19 -0500

    Added binding to watchdog

--- watchdog/dist/worker.js
+++ watchdog/dist/worker.js
@@ -1,9 +1,9 @@
 async function pingServers(event) {
-    let services = await SERVICES.list()
+    let services = await env.SERVICES.list()
     // Loop through keys and generate payload
     for (const key of services.keys) {
-        let value = await SERVICES.get(key.name, {type: "json"}) // Get service
-        await SERVICES.put(value.name, JSON.stringify({
+        let value = await env.SERVICES.get(key.name, {type: "json"}) // Get service
+        await env.SERVICES.put(value.name, JSON.stringify({
             is_up: true,
             is_maintain: value.is_maintain,
             last_up: Date.now(),
