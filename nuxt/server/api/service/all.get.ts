6754a0d54bce1686aff6e22bc2d8ef652558da16
Radison Akerman <rak3rman@gmail.com>
2022-05-06 20:13:28 -0500

    Increased efficiency of watchdog/nuxt KV usage

--- nuxt/server/api/service/all.get.ts
+++ nuxt/server/api/service/all.get.ts
@@ -1,18 +1,4 @@
 export default defineEventHandler(async (event) => {
     // @ts-ignore
-    let services = await SERVICES.list()
-    // Loop through keys and generate payload
-    let payload = [];
-    for (const key of services.keys) {
-        // @ts-ignore
-        let value = await SERVICES.get(key.name, {type: "json"}) // Get service
-        value.name = key.name // Add name to payload
-        delete value.subscribers // Remove subscribers from payload
-        payload.push(value) // Add service to payload array
-    }
-    // Sort services by last_down
-    payload.sort(function(a, b) {
-        return b.last_down - a.last_down;
-    });
-    return payload;
+    return await SERVICES.get('payload', {type: 'json'})
 })
--- watchdog/dist/worker.js
+++ watchdog/dist/worker.js
@@ -1,35 +1,46 @@
 async function pingServers(event, env) {
+    // Get list of service keys from CF KV
     let services = await env.SERVICES.list()
+    let payload = [];
     // Loop through keys and generate payload
     for (const key of services.keys) {
-        // Get service details
-        let value = await env.SERVICES.get(key.name, {type: "json"})
-        // Get request ready
-        const init = {
-            method: 'GET',
-            redirect: value.redirect ? value.redirect : 'manual',
-            // headers: {
-            //     'User-Agent': 'cf-worker-status-page',
-            // },
-        }
+        // Get service details from CF KV
+        let orig_serv = await env.SERVICES.get(key.name, {type: 'json'})
         // Ping server
         const req_start = Date.now()
-        const res = await fetch('https://' + key.name, init)
+        const response = await fetch('https://' + key.name, {
+            method: 'GET',
+            redirect: orig_serv.redirect ? orig_serv.redirect : 'manual',
+        })
         const req_time = Math.round(Date.now() - req_start)
-        const is_up = res.status === 200
-        // Update service details
-        await env.SERVICES.put(key.name, JSON.stringify({
+        const is_up = response.status === 200
+        // Update existing service with updated data
+        let updated_serv = {
             is_up: is_up,
-            is_maintain: value.is_maintain,
-            last_up: is_up ? Date.now() : value.last_up,
-            last_down: is_up ? value.last_down : Date.now(),
+            is_maintain: orig_serv.is_maintain,
+            last_up: is_up ? Date.now() : orig_serv.last_up,
+            last_down: is_up ? orig_serv.last_down : Date.now(),
             trip_time: req_time,
-            last_err_code: res.status,
-            redirect: value.redirect,
-            location: value.location,
-            subscribers: value.subscribers,
-        }))
+            last_err_code: response.status,
+            redirect: orig_serv.redirect,
+            location: orig_serv.location,
+            subscribers: orig_serv.subscribers,
+        }
+        // Update service details to CF KV
+        await env.SERVICES.put(key.name, JSON.stringify(updated_serv))
+        // Push into services payload
+        updated_serv.name = key.name // Add name to payload
+        delete updated_serv.redirect // Remove redirect method from payload
+        delete updated_serv.subscribers // Remove subscribers from payload
+        payload.push(updated_serv) // Add service to payload array
     }
+    // Sort services payload by last_down
+    payload.sort(function(a, b) {
+        return b.last_down - a.last_down;
+    });
+    // Update services payload to CF KV
+    await env.SERVICES.put('payload', JSON.stringify(payload))
+    // Return that we finished
     return true
 }
 
