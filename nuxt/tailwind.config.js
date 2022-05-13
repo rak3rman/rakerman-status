523a86281abba9eb9c5db7478db3edae22d6163b
Radison Akerman <rak3rman@gmail.com>
2022-05-13 14:53:11 -0500

    Upgraded wrangler

--- nuxt/tailwind.config.js
+++ nuxt/tailwind.config.js
@@ -6,14 +6,6 @@ module.exports = {
     "./*.vue",
     "./plugins/**/*.{js,ts}",
   ],
-  theme: {
-    // extend: {
-    //   fontFamily: {
-    //     mono: 'Andale Mono',
-    //     sans: 'Futura'
-    //   },
-    // },
-  },
   plugins: [
     require('daisyui'),
     require('@tailwindcss/forms'),
@@ -21,16 +13,4 @@ module.exports = {
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
-  // daisyui: {
-  //   themes: [
-  //     {
-  //       'pulqra': {
-  //         "primary": "#E8E8E8", // Primary theme color (logo, buttons)
-  //         "primary-content": "#2E2E2E",
-  //         "secondary": "#000000", // Secondary theme color (alt icons)
-  //         "secondary-content": "#2BFB02",
-  //       },
-  //     },
-  //   ],
-  // },
 }
